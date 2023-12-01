a512440bf794e302d088571c4ce0e8f79f7dc1a3
BongBong <8253jang@daum.net>
2023-12-01 15:13:24 +0900

    index.js에 multiply 추가, add 수정

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* index.html을 위한 js파일 */
-function add(a,b){
+function add(a,b,c=10){
+    if(c!==undefined){
+        return a+b+c;
+    }
     return a+b;
+}
+
+function multiply(a,b){
+  retrun a*b;
 }
