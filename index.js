13c7012d3189c56aff5e3fc08fd944c7be0df64c
BongBong <8253jang@daum.net>
2023-12-01 15:17:50 +0900

    index.js 수정, substract함수 추가

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 /* index.html을 위한 js파일 */
-function add(a,b){
-    return a+b;
+function add(a,b,c){
+    if(c!==undefined){
+     return a+b;
+    }
+}
+
+function substract(a,b){
+    if(a>b){
+        return a-b;
+    }else{
+        return b-a;
+    }
 }
